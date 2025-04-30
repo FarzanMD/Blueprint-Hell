4e24de3ae1931977b915e8f9393611a33df4d94d
farzan <farzan.md1384@gmail.com>
2025-04-30 17:13:28 +0330

    Updating game panel to show adding Items

--- src/View/GamePanel.java
+++ src/View/GamePanel.java
@@ -1,17 +1,27 @@
 package View;
+
+import Model.Port;
+import Model.SystemNode;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
+    private final SystemNode testNode;
+
     public GamePanel() {
-        setBackground(Color.DARK_GRAY);
+        setBackground(Color.WHITE);
+
+        testNode = new SystemNode(200, 150, 120, 100);
+        testNode.addInputPort(Port.Type.SQUARE);
+        testNode.addInputPort(Port.Type.TRIANGLE);
+        testNode.addOutputPort(Port.Type.SQUARE);
+        testNode.addOutputPort(Port.Type.TRIANGLE);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // Placeholder drawing
-        g.setColor(Color.WHITE);
-        g.drawString("Game is running...", 50, 50);
+        testNode.draw((Graphics2D) g);
     }
 }
